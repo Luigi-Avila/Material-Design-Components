127870da69b3e04eb82fce9c2b9a7dc2088e3978
Luigi-Avila <l.avila.uvg@gmail.com>
2023-03-22 17:40:20 -0600

    feat: Slider component added

--- app/src/main/java/com/example/material_design_components/ScrollingActivity.kt
+++ app/src/main/java/com/example/material_design_components/ScrollingActivity.kt
@@ -90,6 +90,10 @@ class ScrollingActivity : AppCompatActivity() {
                 binding.fab.hide()
             }
         }
+
+        binding.content.sldVol.addOnChangeListener { slider, value, fromUser ->
+            binding.content.tvSubtitle.text = "Vol: $value"
+        }
     }
 
     private fun loadImage(url: String = "https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec"){
